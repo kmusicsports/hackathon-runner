f5376319e78f44359a8b64784f3d113f2e47f62d
kmusicsports <naokik.musicsports1027@gmail.com>
2024-11-18 08:38:31 +0000

    build: Set up vite, react, typescript, tailwindcss, shadcn/ui, eslint, and biome

---
+++ eslint.config.js
@@ -0,0 +1,83 @@
+import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tailwind from "eslint-plugin-tailwindcss";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      ...tseslint.configs.recommended,
+      ...tailwind.configs["flat/recommended"],
+    ],
+    files: ["*/**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "import/order": [
+        "warn",
+        {
+          alphabetize: {
+            caseInsensitive: true,
+            order: "asc",
+          },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              group: "builtin",
+              pattern: "{react,react-dom/**}",
+              position: "before",
+            },
+            {
+              group: "internal",
+              pattern: "{@/**}",
+              position: "before",
+            },
+            {
+              group: "internal",
+              pattern: "{/**}",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+        },
+      ],
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {},
+        alias: {
+          map: [
+            ['', './public'],
+          ],
+        },
+      },
+    },
+  }
+);
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from "node:path";
+
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "127.0.0.1",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
