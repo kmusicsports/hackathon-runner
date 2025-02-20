e97fc523bd01bc5f6a3acb97375f0b2afc395fb4
kmusicsports <naokik.musicsports1027@gmail.com>
2025-02-20 09:46:33 +0000

    feat(ui): Update home page

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router";
 
 import { Button } from "@/components/ui/button";
+import { appName } from "@/config";
 
 const HomePage = () => {
   return (
-    <div>
-      <div>
-        ハッカソンに対する技術レベルを診断し、「ハッカソンまでに何を準備しておけば良いか」や「ハッカソンでどう振る舞えば良いか」を提案します。
+    <div className="bg-[url(/hr-women.png)] bg-cover bg-center bg-no-repeat text-primary-foreground">
+      <div className="flex h-screen flex-col items-center justify-center space-y-16 bg-[rgba(0,0,0,0.4)]">
+        <div className="flex scroll-m-20 flex-col items-center space-y-5 text-5xl font-extrabold tracking-tight lg:text-6xl">
+          <span>Welcome</span>
+          <span>to</span>
+          <span className="italic">{appName}</span>
+        </div>
+
+        <div className="xs:text-lg flex flex-col items-center text-base sm:text-xl">
+          <span>あなたのハッカソンに対する技術レベルを診断し、</span>
+          <span className="max-w-sm sm:max-w-[30rem] lg:max-w-none">
+            「ハッカソンまでに何を準備しておけば良いか」や「当日はどう振る舞えば良いか」を提案します。
+          </span>
+        </div>
+
+        <Button size={"lg"} asChild>
+          <Link to="/check/question" className="text-xl">
+            さっそく診断
+          </Link>
+        </Button>
       </div>
-      <Button asChild>
-        <Link to="/check/question">さっそく診断する</Link>
-      </Button>
     </div>
   );
 };
