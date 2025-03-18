6d666a92bbd1caa405dbe2f9795857b793a2b8eb
kmusicsports <naokik.musicsports1027@gmail.com>
2025-03-18 05:29:57 +0000

    feat(ui): Add ResultMessageList to ResultPage

--- src/app/pages/check/result.tsx
+++ src/app/pages/check/result.tsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 
+import { H2 } from "@/components/elements/typography/heading";
 import { Button } from "@/components/ui/button";
+import ResultMessageList from "@/features/check/components/result-message-list";
+
+import type { Answers } from "@/features/check/types";
 
 const ResultPage = () => {
+  const [searchParams] = useSearchParams();
+  const answers: Answers = {
+    hackathon: Number(searchParams.get("hackathon")),
+    team: Number(searchParams.get("team")),
+    development: Number(searchParams.get("development")),
+    git: Number(searchParams.get("git")),
+    video: Number(searchParams.get("video")),
+    presentation: Number(searchParams.get("presentation")),
+    design: Number(searchParams.get("design")),
+    frontend: Number(searchParams.get("frontend")),
+    backend: Number(searchParams.get("backend")),
+    infrastructure: Number(searchParams.get("infrastructure")),
+    analysis: Number(searchParams.get("analysis")),
+  };
+
   return (
-    <div>
-      <h1>診断結果</h1>
-      <Button asChild>
+    <div className="mx-auto mt-4 w-1/2 space-y-4">
+      <H2 className="text-center">診断結果</H2>
+
+      <ResultMessageList answers={answers} />
+
+      <Button className="w-full" asChild>
         <Link to="/">ホームへ戻る</Link>
       </Button>
     </div>
---
+++ src/features/check/types/index.ts
@@ -0,0 +1,13 @@
+export type Answers = {
+  hackathon: number;
+  team: number;
+  development: number;
+  git: number;
+  video: number;
+  presentation: number;
+  design: number;
+  frontend: number;
+  backend: number;
+  infrastructure: number;
+  analysis: number;
+};
