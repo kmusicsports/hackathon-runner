1f8937a3a9382def67483b8b2d50c4733542b3e4
kmusicsports <naokik.musicsports1027@gmail.com>
2025-05-18 11:46:34 +0000

    fix: Optimize custom hooks

--- src/hooks/use-counter.ts
+++ src/hooks/use-counter.ts
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-export const useCounter = () => {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => setCount((count) => count + 1);
-  const decrementCount = () => setCount((count) => count - 1);
+export const useCounter = (initialCount = 0) => {
+  const [count, setCount] = useState(initialCount);
 
-  return [count, { setCount, incrementCount, decrementCount }] as const;
+  const incrementCount = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+
+  const decrementCount = useCallback(() => {
+    setCount((prev) => Math.max(0, prev - 1));
+  }, []);
+
+  return [
+    count,
+    {
+      setCount,
+      incrementCount,
+      decrementCount,
+    },
+  ] as const;
 };
